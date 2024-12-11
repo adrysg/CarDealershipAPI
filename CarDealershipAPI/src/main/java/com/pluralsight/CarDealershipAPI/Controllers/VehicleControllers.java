b8145cf0322035c48f0e02646268e2cbdf59d46b
agabin <agabin@appdev.yearup.org>
2024-12-11 16:07:57 -0500

    Working on vehicle controllers class

--- CarDealershipAPI/src/main/java/com/pluralsight/CarDealershipAPI/Controllers/VehicleControllers.java
+++ CarDealershipAPI/src/main/java/com/pluralsight/CarDealershipAPI/Controllers/VehicleControllers.java
@@ -1,5 +1,47 @@
 package com.pluralsight.CarDealershipAPI.Controllers;
 
+import com.pluralsight.CarDealershipAPI.models.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class VehicleControllers {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public VehicleControllers(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Vehicle> getVehicles(){
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        DataSource dataSource;
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement pStatement = connection.prepareStatement("SELECT * FROM cardealership.vehicles;");
+            ResultSet results = pStatement.executeQuery())
+        {
+            while (results.next()){
+                int vehicleVin = results.getInt("VehicleVIN");
+                int year = results.getInt("Year");
+                String make = results.getString("Make");
+                String model = results.getString("Model");
+                String vehicleType = results.getString("VehicleType");
+                String color = results.getString("Color");
+                int odometer = results.getInt("Odometer");
+                double price = results.getDouble("Price");
+
+            }
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return vehicles;
+    }
 
 }
